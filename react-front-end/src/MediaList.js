5f8830a45660d6165b4d0434dcfb0eca3459e667
PowerSlaveAlfons <stefan.alfons@icloud.com>
2022-01-07 21:36:24 +0100

    refactor to use concepts that are above my paygrade (shoutouts to Fredi100)

--- react-front-end/src/MediaList.js
+++ react-front-end/src/MediaList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { concatAll, concatMap, map, zip, from, of, toArray } from 'rxjs';
 
 
 export default class MediaList extends React.Component {
@@ -10,36 +11,47 @@ export default class MediaList extends React.Component {
     }
 
     async componentDidMount() {
-        
+        /*
         await axios.get('https://localhost:5001/media/getAll')
             .then(res => {
                 const Media = res.data;
                 this.setState({ Media });
-            }).catch(err => console.error(err))
+            }).catch(err => console.error(err)) */
 
-        for (const index in this.state.Media) 
-        {
+        from(axios.get('https://localhost:5001/media/getAll')).pipe(
+            map(res => res.data),
+            concatAll(),
+            concatMap(media => {
+                return zip( of(media), axios.get('https://localhost:5001/entries/' + media.id))
+            }),
+            map(([media, res]) => {
+                media["lastDate"] = res.data.map(entry => entry.timestamp).reduce((last, Entry) => Entry > last ? Entry : last, "0001-01-01T00:00:00");
+                return media;
+            }), toArray()
+        ).subscribe({next: Media => this.setState({ Media }), error: err => console.error(err), complete: () => console.info('feddish') });
+        /*
+        for (const index in this.state.Media) {
             await axios.get('https://localhost:5001/entries/' + this.state.Media[index].id)
-            .then(res => {
-                const Entries = res.data;
-                this.setState({ Entries })
-            }).catch(err => console.error(err))
-            console.log("I get here")
+                .then(res => {
+                    const Entries = res.data;
+                    this.setState({ Entries })
+                }).catch(err => console.error(err))
             this.state.Media[index]["lastDate"] = this.state.Entries.map(entry => entry.timestamp).reduce((last, Entry) => Entry > last ? Entry : last, "0001-01-01T00:00:00")
             console.log(index)
         }
         this.setState(this.state.Media)
+        */
     }
 
     render() {
         return (
             <div className='GameList'>
                 <ul>
-                    {this.state.Media.map(Media => 
-                    <Link to={`/media/${Media.id}`}>
-                        <li key={Media.id}>{Media.name}...{Media.lastDate}</li></Link>)}
+                    {this.state.Media.map(Media =>
+                        <Link to={`/media/${Media.id}`}>
+                            <li key={Media.id}>{Media.name}...{Media.lastDate}</li></Link>)}
 
-                    
+
                 </ul>
             </div>
         )
